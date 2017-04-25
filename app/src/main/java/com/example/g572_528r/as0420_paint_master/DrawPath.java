42d0f00a0189a561d9e9d91d53a813fd0afc48da
g572-528r <845828099@qq.com>
2017-04-25 16:31:26 +0800

    paint自定义画板

---
+++ app/src/main/java/com/example/g572_528r/as0420_paint_master/DrawPath.java
@@ -0,0 +1,9 @@
+package com.example.g572_528r.as0420_paint_master;
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+public class DrawPath {
+    public Path path;
+    public Paint paint;
+}
